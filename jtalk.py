3867fa5b722cbaf6a063d7647b34887b81d97ac8
KenziHashimoto <kenzihashimoto@ares.eonet.ne.jp>
2021-02-15 21:04:57 +0900

    Please install Good AI viice

---
+++ jtalk.py
@@ -0,0 +1,36 @@
+﻿#coding: utf-8
+import subprocess
+from datetime import datetime
+import winsound
+
+def jtalk(t):
+    # depend on your install folder
+    OPENJTALK_BINPATH = 'open_jtalk-1.11/bin'
+    OPENJTALK_DICPATH = 'open_jtalk-1.11/mecab-naist-jdic'
+    # VOICEPATH -> can change mei voice
+    OPENJTALK_VOICEPATH = 'mei/mei_normal.htsvoice'
+    open_jtalk=[OPENJTALK_BINPATH + '/open_jtalk.exe']
+    mech=['-x',OPENJTALK_DICPATH]
+    htsvoice=['-m',OPENJTALK_VOICEPATH]
+    speed=['-r','1.0']
+    outwav=['-ow','open_jtalk.wav']
+    cmd=open_jtalk+mech+htsvoice+speed+outwav
+    c = subprocess.Popen(cmd,stdin=subprocess.PIPE)
+
+    # convert text encoding from utf-8 to shitf-jis
+    c.stdin.write(t.encode('shift-jis'))
+    c.stdin.close()
+    c.wait()
+
+    # play wav audio file with winsound module
+    winsound.PlaySound('open_jtalk.wav', winsound.SND_FILENAME)
+
+
+def say_datetime():
+    # get datetime and call jtalk
+    d = datetime.now()
+    text = '%s月%s日、%s時%s分%s秒' % (d.month, d.day, d.hour, d.minute, d.second)
+    jtalk(text)
+
+if __name__ == '__main__':
+    say_datetime()
