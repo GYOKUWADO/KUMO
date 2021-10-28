30e092931e1c5e2b5f0f7d0645dfc71e489db450
Kenzi Hashimoto <kenzihashimoto@ares.eonet.ne.jp>
2021-10-28 13:44:28 +0900

    FIXUP_

--- AI_TALK.bat
+++ AI_TALK.bat
@@ -10,7 +10,7 @@ set LINECOUNT=%LINECOUNT:~1,-1%
 
 echo %LINECOUNT%
 
-REM curl -s -X POST https://api.a3rt.recruit.co.jp/talk/v1/smalltalk -F "apikey=DZZE7NQfPP5wdpwFiw7k7Z5SRjD7tXY3" -F "query=%q%" | jq -c .results[].reply
+REM curl -s -X POST https://api.a3rt.recruit.co.jp/talk/v1/smalltalk -F "apikey=" -F "query=%q%" | jq -c .results[].reply
 
 python jtalk.py %LINECOUNT%
 
