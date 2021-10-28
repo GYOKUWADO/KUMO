6e35d3a9e83d2470d66ec7a344d98404fb95063d
Kenzi Hashimoto <kenzihashimoto@ares.eonet.ne.jp>
2021-10-28 13:47:22 +0900

    FIXUP

--- AI_TALK.bat
+++ AI_TALK.bat
@@ -4,7 +4,7 @@
 
 set /p q="IN THE MSG:"
 
-for /f "usebackq" %%A in (`curl -s -X POST https://api.a3rt.recruit.co.jp/talk/v1/smalltalk -F "apikey=DZZE7NQfPP5wdpwFiw7k7Z5SRjD7tXY3" -F "query=%q%" ^| jq -c .results[].reply`) do set LINECOUNT=%%A
+for /f "usebackq" %%A in (`curl -s -X POST https://api.a3rt.recruit.co.jp/talk/v1/smalltalk -F "apikey=" -F "query=%q%" ^| jq -c .results[].reply`) do set LINECOUNT=%%A
 
 set LINECOUNT=%LINECOUNT:~1,-1%
 
